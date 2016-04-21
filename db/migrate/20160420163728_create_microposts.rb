22ad9e769f1412816988831630b49b1e9f9aee28
masanori_hayashi <884masanori884@gmail.com>
2016-04-21 08:55:44 +0000

    8.4

--- db/migrate/20160420163728_create_microposts.rb
+++ db/migrate/20160420163728_create_microposts.rb
@@ -2,7 +2,7 @@ class CreateMicroposts < ActiveRecord::Migration
   def change
     create_table :microposts do |t|
       t.references :user, index: true, foreign_key: true
-      t.text :coutent
+      t.text :content
 
       t.timestamps null: false
       t.index [:user_id, :created_at]
--- db/schema.rb
+++ db/schema.rb
@@ -11,21 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 20160420163728) do
 
   create_table "microposts", force: :cascade do |t|
     t.integer  "user_id"
-    t.text     "coutent"
+    t.text     "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
   add_index "microposts", ["user_id", "created_at"], name: "index_microposts_on_user_id_and_created_at"
   add_index "microposts", ["user_id"], name: "index_microposts_on_user_id"
-=======
-ActiveRecord::Schema.define(version: 20160420160544) do
->>>>>>> user-profile
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
