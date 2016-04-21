d6012825e8aa1c5b48b92b97f64034c581251319
masanori_hayashi <884masanori884@gmail.com>
2016-04-21 14:31:34 +0000

    add posting microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,8 @@ class UsersController < ApplicationController
   before_action :collect_user, only: [:edit, :update]
   
   def show
+    @user = User.find(params[:id])
+    @microposts = @user.microposts.order(created_at: :desc)
   end
   
   def new
